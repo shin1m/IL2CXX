9798ad3b8335f38dcaf88029a3e678d35118b361
Shin-ichi MORITA <shin1morita@gmail.com>
2021-02-04 19:21:38 +0900

    Migrated recyclone to submodule.

--- IL2CXX/src/recyclone/test/test_empty.cc
+++
@@ -1,9 +0,0 @@
-#include "type.h"
-
-int main(int argc, char* argv[])
-{
-	t_engine<t_type>::t_options options;
-	options.v_verbose = options.v_verify = true;
-	t_engine<t_type> engine(options);
-	return engine.f_exit(0);
-}
